84941d4d5949e590afc675bb2bb378b39b05320d
Richard Todd <richard@waywardcode.com>
2016-07-14 11:20:35 -0500

    modularized code
    The main() had gotten a little long, so I split it into a couple
    functions to make understanding the code easier in years to come.

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ var hlp = flag.Bool("h", false, "display help text")
 var rsize = flag.Uint("rsz", 0, "resize the image by x% instead of video-mode size")
 var zc = flag.Uint("zc", 0, "set the zero color of a CGA mode to EGA color N")
 
+// help puts the usage information on Stderr and exits with a non-zero code.
 func help() {
 	fmt.Fprintln(os.Stderr, "usage: cgaify [-h] [-m MODE] [-rsz PCT] [-zc N] file...")
 	fmt.Fprintln(os.Stderr, "\nOptions:")
@@ -36,11 +37,79 @@ func help() {
 	os.Exit(1)
 }
 
+// globally track the number of errors we encountered
 var errCnt = 0
 
 func disperr(ctxt string, err error) {
 	errCnt++
 	fmt.Fprintf(os.Stderr, "%s: %s\n", ctxt, err.Error())
+}
+
+// resizeImage changes the image dimensions to match the graphics mode,
+// or if the -rsz option was used, it resizes it by the given percentage
+func resizeImage(i image.Image, gmode *mode) image.Image {
+	if *rsize == 100 {
+		// just return the image if the size is unchanged
+		return i
+	}
+
+	srcBounds := i.Bounds()
+	newW, newH := gmode.width, gmode.height
+	if (float64(srcBounds.Dx()) / float64(srcBounds.Dy())) > gmode.aspectRatio() {
+		newH = 0
+	} else {
+		newW = 0
+	}
+
+	// If the user has selected a percentage by which to resize,
+	// forget what we just calculated and use the percentage instead.
+	if *rsize > 0 {
+		newW, newH = uint(float64(srcBounds.Dx())*(float64(*rsize)/100.0)), 0
+	}
+	return resize.Resize(newW, newH, i, resize.Bicubic)
+}
+
+// process takes a filename and a graphics mode, and converts the size and
+// colors to match that mode.
+func process(fname string, gmode *mode) {
+	// STEP ONE: Decode the image...
+	srcfile, err := os.Open(fname)
+	if err != nil {
+		disperr(fname, err)
+		return
+	}
+	srcimg, _, err := image.Decode(srcfile)
+	srcfile.Close()
+	if err != nil {
+		disperr(fname, err)
+		return
+	}
+
+	// STEP TWO: Resize and Re-Palette the image...
+	srcimg = resizeImage(srcimg, gmode)
+	srcBounds := srcimg.Bounds()
+	outimg := image.NewPaletted(srcBounds, gmode.colors)
+	draw.FloydSteinberg.Draw(outimg, srcBounds, srcimg, image.ZP)
+
+	// STEP THREE: Output the new image...
+	outfile, err := os.OpenFile(
+		filepath.Base(fname)+"_"+*gmd+".gif",
+		os.O_WRONLY|os.O_CREATE|os.O_TRUNC,
+		0666)
+	if err != nil {
+		disperr(fname, err)
+		return
+	}
+	err = gif.Encode(
+		outfile,
+		outimg,
+		&gif.Options{len(gmode.colors), nil, nil})
+	outfile.Close()
+	if err != nil {
+		disperr(fname, err)
+		return
+	}
+
 }
 
 func main() {
@@ -58,57 +127,7 @@ func main() {
 	}
 
 	for _, fname := range flag.Args() {
-		srcfile, err := os.Open(fname)
-		if err != nil {
-			disperr(fname, err)
-			continue
-		}
-
-		srcimg, _, err := image.Decode(srcfile)
-		srcfile.Close()
-		if err != nil {
-			disperr(fname, err)
-			continue
-		}
-		srcBounds := srcimg.Bounds()
-
-		// resize image ...
-		if *rsize != 100 {
-			newW, newH := gmode.width, gmode.height
-			if (float64(srcBounds.Dx()) / float64(srcBounds.Dy())) > gmode.aspectRatio() {
-				newH = 0
-			} else {
-				newW = 0
-			}
-
-			// If the user has selected a percentage by which to resize,
-			// forget what we just calculated and use the percentage instead.
-			if *rsize > 0 {
-				newW, newH = 0, uint(float64(srcBounds.Dy())*(float64(*rsize)/100.0))
-			}
-			srcimg = resize.Resize(newW, newH, srcimg, resize.Bicubic)
-			srcBounds = srcimg.Bounds()
-		}
-		outimg := image.NewPaletted(srcBounds, gmode.colors)
-		draw.FloydSteinberg.Draw(outimg, srcBounds, srcimg, image.ZP)
-
-		outfile, err := os.OpenFile(
-			filepath.Base(fname)+"_"+*gmd+".gif",
-			os.O_WRONLY|os.O_CREATE|os.O_TRUNC,
-			0666)
-		if err != nil {
-			disperr(fname, err)
-			continue
-		}
-		err = gif.Encode(
-			outfile,
-			outimg,
-			&gif.Options{len(gmode.colors), nil, nil})
-		outfile.Close()
-		if err != nil {
-			disperr(fname, err)
-			continue
-		}
+		process(fname, gmode)
 	}
 
 	if errCnt > 0 {
